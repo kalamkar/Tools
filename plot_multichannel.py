dfe824d21a6a987ef403f36dbea9f2c7c1894585
Abhijit Kalamkar <abhi@Abhijits-MacBook-Air-2.local>
2016-12-06 12:30:12 -0800

    Added bessel bandpass filter and detrending the signal to show it side by side with filtered signal

--- plot_multichannel.py
+++ plot_multichannel.py
@@ -7,14 +7,36 @@ Created on Dec 02, 2016
 import  struct
 import sys
 import numpy as np
+from scipy import signal
 from matplotlib import pyplot
+
+SAMPLING_RATE = 200
+MAX_Y = 200000
+MAX_X = 3600
+MIN_X = 50
+FACTOR = 50
 
 
 def main():
     filename = sys.argv[1]
     channel1, channel2 = read(filename)
-    plot(channel1, channel2)
+    channel1 = signal.detrend(channel1)
+    channel2 = signal.detrend(channel2)
+
+    figure = pyplot.figure(figsize=(15, 6))
+
+    # plot(figure, channel1, 'b')
+    # plot(figure, np.array(bessel(channel1)) * FACTOR, 'lightblue')
+
+    plot(figure, channel2, 'g')
+    plot(figure, np.array(bessel(channel2)) * FACTOR, 'lightgreen')
+
     pyplot.show()
+
+
+def bessel(data):
+    b, a = signal.bessel(1, [rad_per_s(0.5), rad_per_s(840)], btype='bandpass')
+    return signal.lfilter(b, a, data)
 
 
 def read(filename):
@@ -39,15 +61,15 @@ def parse_int(value):
         return -1
 
 
-def plot(channel1, channel2):
-    fig = pyplot.figure(figsize=(15, 6))
+def plot(figure, channel, color):
+    chart = figure.add_subplot(1, 1, 1)
+    chart.set_xticks(np.arange(0, len(channel), SAMPLING_RATE))
+    chart.plot(channel, linewidth=1, color=color)
+    chart.axis([MIN_X, MAX_X, -MAX_Y, MAX_Y])
 
-    ch1 = fig.add_subplot(1, 1, 1)
-    ch1.plot(channel1, linewidth=1)
 
-    ch2 = fig.add_subplot(1, 1, 1)
-    ch2.plot(channel2, linewidth=1)
-
+def rad_per_s(hertz):
+    return 2 * 3.14159265 * hertz
 
 if __name__ == "__main__":
     main()
