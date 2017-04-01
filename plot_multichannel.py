7aa8fd028f2f80fc57cd76e9a38d8e25eafc837c
Abhijit Kalamkar <abhi@Abhijits-Air-2.lan>
2017-04-01 09:49:13 -0700

    Added new visualizations, slopes seem promising

--- plot_multichannel.py
+++ plot_multichannel.py
@@ -13,7 +13,7 @@ from matplotlib import pyplot
 
 SAMPLING_RATE = 51.2
 DOWN_SAMPLE_FACTOR = 30
-POLY_FIT_WINDOW = 500
+POLY_FIT_WINDOW = 50
 MEDIAN_WINDOW = 20
 
 START = int(SAMPLING_RATE * 15)  # Ignore first 15 seconds
@@ -28,10 +28,12 @@ def main():
     # show_algo_realtime(columns, figure)
     # show_algo_chunks(columns, figure)
     # show_algo(columns, figure)
-    show_raw(columns, figure)
     # show_detrend(columns, figure)
+    # show_remove_slope(columns, figure)
+    show_slope(columns, figure)
+    # show_raw(columns, figure)
     # show_filtered(columns, figure)
-    # show_raw_filtered(columns, figure)
+    # show_filtered_slice(columns, figure)
 
     pyplot.show()
 
@@ -41,12 +43,12 @@ def show_algo_realtime(columns, figure):
     channel1 = np.array(columns[0][START:]).astype(np.int)
     channel11, channel12 = process_realtime(channel1, 200)
     plot(figure, 211, channel11, 'lightblue', max_y=max_y, start=3000-START)
-    plot(figure, 211, channel12, 'blue', max_y=max_y, start=3000-START)
+    plot(figure, 211, channel12, 'blue', max_y=max_y, start=3000-START, twin=True)
 
     channel2 = np.array(columns[1][START:]).astype(np.int)
     channel21, channel22 = process_realtime(channel2, 200)
     plot(figure, 212, channel21, 'lightgreen', max_y=max_y, start=3000-START)
-    plot(figure, 212, channel22, 'green', max_y=max_y, start=3000-START)
+    plot(figure, 212, channel22, 'green', max_y=max_y, start=3000-START, twin=True)
 
 
 def show_algo_chunks(columns, figure):
@@ -80,40 +82,73 @@ def show_algo(columns, figure):
 
 
 def show_filtered(columns, figure):
-    channel1 = columns[2][START:]
-    channel2 = columns[3][START:]
+    channel1 = columns[2][START:-50]
+    channel2 = columns[3][START:-50]
 
-    plot(figure, 211, channel1, 'blue',  max_y=20000, start=3000-START)
-    plot(figure, 212, channel2, 'green',  max_y=20000, start=3000-START)
+    plot(figure, 211, channel1, 'blue', start=3000-START)
+    plot(figure, 212, channel2, 'green', start=3000-START)
 
 
-def show_raw_filtered(columns, figure):
-    max_y = 20000
+def show_filtered_slice(columns, figure):
+    channel1 = columns[2][7000:9000]
+    channel2 = columns[3][7000:9000]
 
-    channel1 = signal.detrend(columns[0][START:])
-    channel2 = signal.detrend(columns[1][START:])
+    plot(figure, 211, channel1, 'blue', window=len(channel1))
+    plot(figure, 212, channel2, 'green', window=len(channel2))
 
-    plot(figure, 211, columns[2][START:], 'blue',  max_y=max_y, start=3000-START)
-    plot(figure, 212, columns[3][START:], 'green',  max_y=max_y, start=3000-START)
 
-    plot(figure, 211, channel1, 'lightblue',  max_y=max_y, start=3000-START)
-    plot(figure, 212, channel2, 'lightgreen',  max_y=max_y, start=3000-START)
+def show_remove_slope(columns, figure):
+    raw1 = np.array(columns[0][START:-50]).astype(np.int)
+    raw2 = np.array(columns[1][START:-50]).astype(np.int)
+
+    channel1 = remove_slope(raw1)[7000:9000]
+    channel2 = remove_slope(raw2)[7000:9000]
+
+    raw1 = signal.detrend(raw1)[7000:9000]
+    raw2 = signal.detrend(raw2)[7000:9000]
+
+    plot(figure, 211, raw1, 'lightblue', window=len(raw1))
+    plot(figure, 211, channel1, 'blue', window=len(channel1), twin=True)
+    plot(figure, 212, raw2, 'lightgreen', window=len(raw2))
+    plot(figure, 212, channel2, 'green', window=len(channel2), twin=True)
+
+
+def show_slope(columns, figure):
+    raw1 = np.array(columns[0][START:-50]).astype(np.int)
+    raw2 = np.array(columns[1][START:-50]).astype(np.int)
+
+    slope1 = get_slopes(raw1)[9000:9500]
+    slope2 = get_slopes(raw2)[9000:9500]
+
+    raw1 = signal.detrend(raw1)[9000:9500]
+    raw2 = signal.detrend(raw2)[9000:9500]
+
+    plot(figure, 211, raw1, 'lightblue', window=len(raw1))
+    plot(figure, 211, slope1, 'blue', window=len(slope1), twin=True)
+    plot(figure, 212, raw2, 'lightgreen', window=len(raw2))
+    plot(figure, 212, slope2, 'green', window=len(slope2), twin=True)
 
 
 def show_raw(columns, figure):
-    channel1 = columns[0][START:-50]
-    channel2 = columns[1][START:-50]
+    raw1 = np.array(columns[0][START:-50]).astype(np.int)
+    raw2 = np.array(columns[1][START:-50]).astype(np.int)
 
-    plot(figure, 211, channel1, 'blue', window=len(channel1))
-    plot(figure, 212, channel2, 'green', window=len(channel2))
+    raw1 = signal.detrend(raw1)
+    raw2 = signal.detrend(raw2)
+
+    plot(figure, 211, raw1, 'blue', window=len(raw1))
+    plot(figure, 212, raw2, 'green', window=len(raw2))
 
 
 def show_detrend(columns, figure):
-    channel1 = signal.detrend(columns[0][START:-50])
-    channel2 = signal.detrend(columns[1][START:-50])
+    channel1 = np.array(columns[0][START:-50]).astype(np.int)
+    channel2 = np.array(columns[1][START:-50]).astype(np.int)
 
-    plot(figure, 211, channel1, 'blue', window=len(channel1))
-    plot(figure, 212, channel2, 'green', window=len(channel2))
+    channel1 = signal.detrend(channel1, bp=range(0, len(channel1), POLY_FIT_WINDOW))
+    channel2 = signal.detrend(channel2, bp=range(0, len(channel1), POLY_FIT_WINDOW))
+
+    plot(figure, 211, channel1, 'blue', start=3000-START)
+    plot(figure, 212, channel2, 'green', start=3000-START)
 
 
 def diffdiff(data):
@@ -155,7 +190,7 @@ def median_filter(data):
     return filtered
 
 
-def remove_drift(data):
+def remove_drift(data, degree=2):
     filtered = []
     for i in range(0, len(data) - POLY_FIT_WINDOW, POLY_FIT_WINDOW):
         curve = get_curve(data[i:i + POLY_FIT_WINDOW])
@@ -168,20 +203,44 @@ def remove_drift(data):
     return filtered
 
 
-def get_curve(data):
+def get_curve(data, degree=2):
     x = []
     y = []
     for i in range(0, len(data)):
         if i % DOWN_SAMPLE_FACTOR == 0:
             x.append(i)
             y.append(data[i])
-    return np.polyfit(x, y, 2)
+    return np.polyfit(x, y, degree)
+
+
+def remove_slope(data):
+    filtered = []
+    slope = get_slope(data[:POLY_FIT_WINDOW])
+    for i in range(POLY_FIT_WINDOW, len(data)):
+        slope = get_slope(data[i - POLY_FIT_WINDOW:i])
+        filtered.append(data[i] - (i * slope))
+    return filtered
+
+
+def get_slopes(data):
+    slopes = [0] * POLY_FIT_WINDOW
+    for i in range(POLY_FIT_WINDOW, len(data)):
+        slope = get_slope(data[i - POLY_FIT_WINDOW:i])
+        slopes.append(slope)
+    return slopes
+
+
+def get_slope(data):
+    median_size = max(1, POLY_FIT_WINDOW / 20) # 5%
+    start = int(np.median(data[:median_size]))
+    end = int(np.median(data[-median_size:]))
+    return float((end - start) / len(data))
 
 
 def process_chunks(data, cutoff):
     stage1 = []
     for i in range(0, len(data) - POLY_FIT_WINDOW, POLY_FIT_WINDOW):
-        curve = get_curve(data[i:i + POLY_FIT_WINDOW])
+        curve = get_curve(data[i:i + POLY_FIT_WINDOW], degree=1)
         chunk = []
         for j in range(i, i + POLY_FIT_WINDOW):
             chunk.append(data[j] - np.polyval(curve, i + j))
@@ -217,7 +276,7 @@ def process_realtime(data, cutoff):
     for i in range(0, len(data)):
         raw = raw[1:]
         raw.append(data[i])
-        curve = get_curve(raw)
+        curve = get_curve(raw, degree=1)
 
         straight = straight[1:]
         straight.append(raw[len(raw) - 1] - np.polyval(curve, len(raw)))
@@ -237,8 +296,11 @@ def process_realtime(data, cutoff):
     return stage1, stage2
 
 
-def plot(figure, row_col, data, color, max_y=0, start=0, window=SAMPLING_RATE * 60):
+def plot(figure, row_col, data, color, max_y=0, start=0,
+         window=SAMPLING_RATE * 60, twin=False):
     chart = figure.add_subplot(row_col)
+    if twin:
+        chart = chart.twinx()
     chart.set_xticks(np.arange(0, len(data), window / 15))
     chart.plot(data, linewidth=1, color=color)
     chart.set_xbound([start, start + window])
